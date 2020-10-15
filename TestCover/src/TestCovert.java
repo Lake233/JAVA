2a1d802a48f4adaf7cae7ffbe54ff6a465331961
Lake233 <3081192900@qq.com>
2020-10-15 11:07:00 +0800

    TestCover file

---
+++ TestCover/src/TestCovert.java
@@ -0,0 +1,23 @@
+class SuperClass{
+    int x;
+    void setX(){
+        x = 10;
+    }
+}
+
+class SubClass extends SuperClass{
+    int x;
+    void setX(){
+        x = 100;
+    }
+    String getMessage(){
+        return "super.x = " + super.x + "\tsub.x = " + x;
+    }
+}
+public class TestCovert {
+    public static void main(String[] args) {
+        SubClass sc = new SubClass();
+        sc.setX();
+        System.out.println(sc.getMessage());
+    }
+}
